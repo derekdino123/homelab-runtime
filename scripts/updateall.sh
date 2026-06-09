be6509e8765782302b99f09fa3b93e2f6abd9142
derekdino123 <37026989+derekdino123@users.noreply.github.com>
2026-06-09 03:45:35 -0400

    Update updateall.sh
    corrected update script names

--- scripts/updateall.sh
+++ scripts/updateall.sh
@@ -4,9 +4,9 @@ echo "========================================"
 echo " FULL SYSTEM UPDATE"
 echo "========================================"
 
-bash /opt/shared/scripts/update-system.sh
+bash /opt/shared/scripts/update.sh
 echo
-bash /opt/shared/scripts/update-repo.sh
+bash /opt/shared/scripts/updategit.sh
 
 echo
 echo "========================================"
